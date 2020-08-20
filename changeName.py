5b44c5e8b2c9c49ff712aad7046d27160ec8e733
Sunu Ilham Pradika <sunudika@gmail.com>
2020-08-20 10:17:07 +0700

    Automatization

--- changeName.py
+++ changeName.py
@@ -1,14 +1,25 @@
 import os 
 
 def main(): 
-    folder = input("Masukkan nama folder sekaligus file: ")
+    folders = [ 'aien', 'alif', 'ba', 'ta', 'dal', 'dhod', 'dzal',
+                'dzho', 'fa', 'ghoin', 'ha', 'ha\'', 'hamzah', 'jiem',
+                'kaf', 'kho', 'lam', 'lamalief', 'miem', 'nun', 'qof',
+                'ro', 'shod', 'sien', 'syien', 'ta', 'tho', 'tsa', 
+                'wawu', 'ya', 'zain']
+    #folder = input("Masukkan nama folder sekaligus file: ")
+    for folder in folders:
+        for count, filename in enumerate(os.listdir(folder)): 
+            try:
+                dst = folder + "_" + str(count) + ".jpg"
+                # dst = 'eka' + "_" + str(count) + ".jpg"
+                src = folder + '\\' + filename 
+                dst = folder + '\\' +  dst
 
-    for count, filename in enumerate(os.listdir(folder)): 
-        dst = folder + "_" + str(count) + ".jpg"
-        src = folder + '\\' + filename 
-        dst = folder + '\\' + dst 
-
-        os.rename(src, dst) 
+                os.rename(src, dst) 
+            except:
+                print("File Sudah Ada")
+            finally:
+                pass
 
 if __name__ == '__main__': 
     main() 
